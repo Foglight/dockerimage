6bed3ce7200535af28cdae7e0e90dd3e55989fdd
ZakeGuo <36032068+ZakeGuo@users.noreply.github.com>
2019-11-27 16:03:58 +0800

    Update docker-entrypoint.sh

--- FglAMDockerImage/docker-entrypoint.sh
+++ FglAMDockerImage/docker-entrypoint.sh
@@ -7,10 +7,10 @@ else
   FMS_URL="${FMS_URL:-default_fms_url}"
   HOST_DISPLAY_NAME="${HOST_DISPLAY_NAME:-default_host_display_name}"
   AUTH_TOKEN="${AUTH_TOKEN:-default_auth_token}"
-  eval "sed -i -r 's@installer.fms=url=https://.*:8443,ssl-allow-self-signed=true,ssl-cert-common-name=quest.com@installer.fms=url=https://$FMS_URL:8443,ssl-allow-self-signed=true,ssl-cert-common-name=quest.com@' /FglAM592/installer.properties"
-  eval "sed -i -r 's@installer.host-display-name=.*@installer.host-display-name=$HOST_DISPLAY_NAME@' /FglAM592/installer.properties"
-  eval "sed -i -r 's@installer.auth-token=.*@installer.auth-token=$AUTH_TOKEN@' /FglAM592/installer.properties"
-  /FglAM592/FglAM-5_9_2-linux-x86_64.bin
+  eval "sed -i -r 's@installer.fms=url=https://.*:8443,ssl-allow-self-signed=true,ssl-cert-common-name=quest.com@installer.fms=url=https://$FMS_URL:8443,ssl-allow-self-signed=true,ssl-cert-common-name=quest.com@' /FglAM/installer.properties"
+  eval "sed -i -r 's@installer.host-display-name=.*@installer.host-display-name=$HOST_DISPLAY_NAME@' /FglAM/installer.properties"
+  eval "sed -i -r 's@installer.auth-token=.*@installer.auth-token=$AUTH_TOKEN@' /FglAM/installer.properties"
+  /FglAM/FglAM-{version}-linux-x86_64.bin
 fi
 
 # Always output the latest log
